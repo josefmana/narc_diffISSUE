1bcfacc2e18dcd07661fdd6c6759bd9bfb342cbf
josefmana <josef.mana@protonmail.com>
2023-04-03 22:33:46 +0200

    most (if not all) analyses ...

--- narc_diffISSUE.R
+++ narc_diffISSUE.R
@@ -4,7 +4,7 @@ setwd( dirname(rstudioapi::getSourceEditorContext()$path) )
 # list packages to use
 pkgs <- c( "tidyverse", "dplyr", # data wrangling
            "ggplot2", "patchwork", # plotting
-           "performance", # model checking
+           "performance", "emmeans", # stats
            "openxlsx" # reading and writing .xlsx
            )
 
@@ -47,7 +47,7 @@ for ( i in names(d0) ) {
   }
 }
 
-# participant 832-39 (d0$nt1) has no difficulty reported but coded as NA, let's assume it should've been 0 instead
+# participant 832-39 (d0$nt1) has no difficulty reported but is coded as NA, let's assume it should've been 0 instead
 d0$nt1[ d0$nt1$record.ID == "832-39", "diff_no.1" ] <- 0
 
 # prepare a data sets for the long-format data
@@ -113,10 +113,11 @@ write.table( t2, file = "tabs/table_2.csv", sep = ",", row.names = F, quote = F,
 # ---- prepare data frame suitable for regressions ----
 
 # list outcomes and predictors of interest
-outs <- d.leg[c(2,3,6),"label"]
-prds <- c("ESS","FSS","NSS")
+out.nms <- d.leg[c(2,3,6),"label"]
+out.vrs <- gsub( " ", "_", out.nms )
+preds <- c("ESS","FSS","NSS")
 
-# prepared the data frame by keeping only the first row for each patient in d1
+# prepare the data frame by keeping only the first row for each patient in d1
 d2 <- d1[ d1$difficulty == "diff_no.1", -which( colnames(d1) %in% c("difficulty","index","label") ) ] %>% as.data.frame()
 
 # for each difficulty from d.leg add an indicator variable for each patient
@@ -127,24 +128,134 @@ for ( i in d.leg$label ) {
   }
 }
 
+# add total number of difficulties reported by each patient
+d2$diff_count <- d2[ , colnames(d2) %in% d.leg$label[-1] ] %>% rowSums()
+
+# prepare columns with underscores instead of spaces for outcomes (such that formula in glm() doesn't have problems)
+for (i in 1:length(out.nms) ) d2[ , out.vrs[i] ] <- d2[ , out.nms[i] ]
+
 # split the data frame by diagnosis
-d2 <- split( d2, d2$dg )
+d3 <- split( d2, d2$dg )
 
 # prepare a table for scaling values of questionnaires
-scl <- expand.grid( dg = unique(d1$dg), quest = prds, M = NA, SD = NA )
+scl <- expand.grid( dg = unique(d1$dg), quest = preds, M = NA, SD = NA )
 
 # add means and standard deviation
-for ( i in 1:nrow(scl) ) scl[ i, c("M","SD") ] <- c( mean( d2[[ as.character(scl$dg[i]) ]][ , as.character(scl$quest[i]) ], na.rm = T ),
-                                                     sd( d2[[ as.character(scl$dg[i]) ]][ , as.character(scl$quest[i]) ], na.rm = T ) )
+for ( i in 1:nrow(scl) ) scl[ i, c("M","SD") ] <- c( mean( d3[[ as.character(scl$dg[i]) ]][ , as.character(scl$quest[i]) ], na.rm = T ),
+                                                     sd( d3[[ as.character(scl$dg[i]) ]][ , as.character(scl$quest[i]) ], na.rm = T ) )
 
 # save the table of scaling values
 write.table( scl, file = "tabs/scaling_table.csv", sep = ",", row.names = F, quote = F, na = "-" )
 
 # scale the questionnaires before analyses
-for ( i in 1:nrow(scl) ) d2[[ as.character(scl$dg[i]) ]][ , paste0( as.character(scl$quest[i]), "_sc" ) ] <- ( d2[[ as.character(scl$dg[i]) ]][ , as.character(scl$quest[i]) ] - scl[i,"M"] ) / scl[i,"SD"]
+for ( i in 1:nrow(scl) ) d3[[ as.character(scl$dg[i]) ]][ , paste0( as.character(scl$quest[i]), "_sc" ) ] <- ( d3[[ as.character(scl$dg[i]) ]][ , as.character(scl$quest[i]) ] - scl[i,"M"] ) / scl[i,"SD"]
 
 
-# ---- compute a set of logistic regressions ----
+# ---- compute a set of logistic (multiple) regressions ----
+
+# prepare a table for results of multiple regression analyses (all predictors at once)
+t3 <- expand_grid( dg = names(d3), out = out.vrs, pred = paste0(preds,"_sc") ) %>%
+  # add columns for the results
+  mutate( b = NA, b_se = NA, z = NA, p = NA, OR = NA, OR_CI95 = NA ) %>% as.data.frame()
+
+# prepare a list for models
+m <- list()
+
+# fit logistic (multiple) regressions
+for ( i in names(d3) ) {
+  for ( j in out.vrs ) m[[i]][[j]] <- glm( formula = paste0( j, " ~ 1 + ESS_sc + FSS_sc", if( i != "IH") " + NSS_sc" ) %>% as.formula,
+                                        data = d3[[i]], family = binomial()
+                                        )
+}
+
+# fill-in values into t3
+for ( i in 1:nrow(t3) ) {
+  
+  # fill-in results on the logit scale
+  tryCatch(
+    t3[ i , c("b","b_se","z","p") ] <- summary( m[[t3$dg[i]]][[t3$out[i]]] )$coefficients[ t3$pred[i], ] %>% round(3) %>% sprintf( "%.3f", . ),
+    error = function(e) { print("There ain't no NSS for IH") } # error message for missing parameters
+  )
+  
+  # add odd ratios with their 95% CIs
+  tryCatch(
+    t3[ i , c("OR","OR_CI95") ] <- c(
+      # estimate
+      exp( summary( m[[t3$dg[i]]][[t3$out[i]]] )$coefficients[ t3$pred[i], 1 ] ) %>% round(2) %>% sprintf( "%.2f", . ),
+      # 95 % CI
+      paste0( "[", exp( summary( m[[t3$dg[i]]][[t3$out[i]]] )$coefficients[ t3$pred[i], 1 ] + qnorm( c(.025,.975) ) * summary( m[[t3$dg[i]]][[t3$out[i]]] )$coefficients[ t3$pred[i], 2 ] ) %>% round(2) %>% sprintf( "%.2f", . ) %>% paste( collapse = ", " ) , "]" )
+    ), error = function(e) { print("There ain't no NSS for IH") } # error message for missing parameters
+  )
+  
+}
+
+
+# ---- compute a set of logistic (simple) regressions ----
+
+# prepare a table for results of multiple regression analyses (all predictors at once)
+t4 <- expand_grid( dg = names(d3), out = out.vrs, pred = paste0(preds,"_sc") ) %>%
+  # add columns for the results
+  mutate( b = NA, b_se = NA, z = NA, p = NA, OR = NA, OR_CI95 = NA ) %>% as.data.frame()
+
+# prepare a list for models
+m1 <- list()
+
+# fit logistic (simple) regressions
+for ( i in names(d3) ) {
+  for ( j in out.vrs ) {
+    for ( k in paste0(preds,"_sc") ) {
+      
+      # don't try to estimate outcome ~ NSS in IH (no data)
+      if ( !(i == "IH" & k == "NSS_sc") ) {
+        
+        # otherwise go for it
+        m1[[i]][[j]][[k]] <- glm( formula = paste0( j, " ~ 1 + ", k ) %>% as.formula() , data = d3[[i]], family = binomial() )
+        
+      }
+    }
+  }
+}
+
+
+
+# fill-in values into t4
+for ( i in 1:nrow(t4) ) {
+  
+  # fill-in results on the logit scale
+  tryCatch(
+    t4[ i , c("b","b_se","z","p") ] <- summary( m1[[t4$dg[i]]][[t3$out[i]]][[t3$pred[i]]] )$coefficients[ t3$pred[i], ] %>% round(3) %>% sprintf( "%.3f", . ),
+    error = function(e) { print("There ain't no NSS for IH") } # error message for missing parameters
+  )
+  
+  # add odd ratios with their 95% CIs
+  tryCatch(
+    t4[ i , c("OR","OR_CI95") ] <- c(
+      # estimate
+      exp( summary( m1[[t3$dg[i]]][[t3$out[i]]][[t3$pred[i]]] )$coefficients[ t3$pred[i], 1 ] ) %>% round(2) %>% sprintf( "%.2f", . ),
+      # 95 % CI
+      paste0( "[", exp( summary( m1[[t3$dg[i]]][[t3$out[i]]][[t3$pred[i]]] )$coefficients[ t3$pred[i], 1 ] + qnorm( c(.025,.975) ) * summary( m[[t3$dg[i]]][[t3$out[i]]] )$coefficients[ t3$pred[i], 2 ] ) %>% round(2) %>% sprintf( "%.2f", . ) %>% paste( collapse = ", " ) , "]" )
+    ), error = function(e) { print("There ain't no NSS for IH") } # error message for missing parameters
+  )
+  
+}
+
+
+# ---- add flags for a liberal p < .05 ----
+
+# apply via assign(), use get(i) to call an object via its name written as a string - super handy!
+for ( i in c("t3","t4") ) {
+  assign( i, get(i) %>% mutate( flag = ifelse( p < .05, ":-)", "" ) ) ) # do the thing
+  write.table( get(i), paste0("tabs/", ifelse( i == "t3", "table_3_multiple_regressions.csv", "table_4_simple_regressions.csv" ) ), sep = ";", row.names = F, quote = F, na = "" ) # write as .csv
+}
+
+
+# ---- compare mean number of difficulties across diagnoses ----
+
+# first check means and SDs per group
+sapply( names(d3), function(i) paste0( round( mean(d3[[i]]$diff_count), 2 ), " ± ", round( sd(d3[[i]]$diff_count), 2 ) ) )
+
+# now compute ANOVA
+anova( lm( diff_count ~ dg, data = d2 ) )
 
 
 # ---- write down session info ----
